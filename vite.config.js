290f86f501415e9213fe0d469c9ac9715b777614
Harnoor Kaur (Intern - DHarnoor.KaurAI/Hosur/TVSMotor) <Harnoor.Kaur@tvsmotor.com>
2025-07-25 15:55:49 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/tvsm-frontend/', 
+  base: '/fe_tvs/', 
 })
